a1bfa3cec3a8685881837f3713e6119f8a22d4f4
yyexela <yyexela@disroot.org>
2019-10-10 14:47:44 -0600

    queueByHeight.cpp works up to test 27/37 before Time Limit Exceeded

--- QueueByHeight/queueByHeight.cpp
+++ QueueByHeight/queueByHeight.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+bool DEBUG = false;
+
 void print(vector<vector<int>> vec){
 	for(vector<vector<int>>::iterator i = vec.begin(); i != vec.end(); ++i){
 		cout<<"[ ";
@@ -14,11 +16,62 @@ void print(vector<vector<int>> vec){
 	cout<<endl;
 }
 
+void print(vector<int> vec){
+	cout<<"[ ";
+	for(vector<int>::iterator i = vec.begin(); i != vec.end(); i++){
+		cout<<*i<<" ";
+	}
+	cout<<"] ";
+}
+
 vector<int> makePerson(int h, int k){
 	vector<int> temp;
 	temp.push_back(h);
 	temp.push_back(k);
 	return temp;
+}
+
+vector<vector<int>> reconstructQueue(vector<vector<int>> &people){
+	if(people.size() == 0){
+	       vector<vector<int>> empty;
+	       return empty;
+	}
+	//vector to return
+	bool swap = false;
+	do{
+		swap = false;
+		if(DEBUG) print(people);
+		//int sorted changes if the back most person is in order
+		int sorted = 1;
+		for(vector<vector<int>>::iterator i = (people.end()-sorted); i != people.begin()-1; --i){
+			int count = 0;
+			if(DEBUG) cout<<"starting from end"<<endl;
+			for(vector<vector<int>>::iterator j = people.begin(); j != i; ++j){
+				if((*j).at(0) >= (*i).at(0)) ++count;
+			}
+			if(DEBUG){
+				cout<<"count for ";
+				if(DEBUG) print(*i);
+				cout<<" is: "<<count<<endl;
+			}
+			if(i == people.end()-sorted && count == (*i).at(1)){
+				if(DEBUG) cout<<"last element in right position"<<endl;
+			        sorted++;
+			} else if(count > (*i).at(1) && count > 0){
+				if(DEBUG) cout<<"swap"<<endl;
+				(*i).swap(*(i-1));
+				if(DEBUG) print(people);
+				swap = true;
+			} else if (count < (*i).at(1) && i+1 != people.end()){
+				if(DEBUG) cout<<"swap back"<<endl;
+				(*i).swap(*(i+1));
+				if(DEBUG) print(people);
+				swap = true;
+			}
+		}
+	} while (swap);
+	if(DEBUG) print(people);
+	return people;
 }
 
 int main(){
@@ -32,4 +85,7 @@ int main(){
 	given.push_back(makePerson(5, 2));
 	cout<<"Initial vector:"<<endl;
 	print(given);
+
+	cout<<endl<<"Final: "<<endl;
+	print(reconstructQueue(given));
 }
