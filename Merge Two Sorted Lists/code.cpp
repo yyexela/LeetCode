00cff5aec2ca164c598efde431cc342357f108f9
yyexela <yyexela@disroot.org>
2019-10-11 08:45:23 -0600

    Problem 21: code set-up

---
+++ Merge Two Sorted Lists/code.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+
+}
+
+int main(){
+
+}
