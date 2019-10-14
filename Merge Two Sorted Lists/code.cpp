bee7b2eb1da2e55680d73c7fba310fb563903a92
yyexela <yyexela@disroot.org>
2019-10-13 21:25:28 -0600

    added print statement and initial lists to test

--- Merge Two Sorted Lists/code.cpp
+++ Merge Two Sorted Lists/code.cpp
@@ -8,10 +8,37 @@ struct ListNode {
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+void printList(ListNode* head){
+	ListNode *tmp = head;
+	while(tmp->next != NULL){
+		cout<<tmp->val<<"->";
+		tmp = tmp->next;
+	}
+	cout<<tmp->val<<endl;
+}
 
+void mergeTwoLists(ListNode* l1, ListNode* l2){
+	cout<<"Called"<<endl;
+	printList(l1);
+	printList(l2);
 }
 
 int main(){
+	cout<<"Making listnodes"<<endl;
+	ListNode *node1 = new ListNode(2);
+	ListNode *node2 = new ListNode(4);
+	node1->next = node2;
+	node2 = node1;
+	node1 = new ListNode(1);
+	node1->next = node2;
+	ListNode *head1 = node1;
 
+	node1 = new ListNode(1);
+	ListNode *head2 = node1;
+	node2 = new ListNode(3);
+	node1->next = node2;
+	node1 = new ListNode(4);
+	node2->next = node1;
+
+	mergeTwoLists(head1, head2);
 }
