2566a836bedda4aa92d6799df14e84d7bb4bb513
yyexela <yyexela@disroot.org>
2019-10-19 23:45:09 -0600

    working problem 26

--- Remove Duplicates from Sorted Array/code.cpp
+++ Remove Duplicates from Sorted Array/code.cpp
@@ -12,6 +12,31 @@ void print(vector<int> vec){
 	cout<<endl;
 }
 
+int removeDuplicates(vector<int>& nums) {
+    if(nums.size() <= 0) return 0;
+    int prev = nums.at(0);
+    for(int i = 1; i < nums.size(); ++i){
+        if(nums.at(i) == prev){
+            nums.erase(nums.begin() + i);
+            cout<<prev<<endl;
+            --i;
+        } else {
+            prev = nums.at(i);
+        }
+    }
+    return nums.size();
+}        
+
 int main(){
-    cout<<"program start"<<endl;
+    vector<int> vec; // [1,1,1,2,3,3]
+    vec.push_back(1);
+    vec.push_back(1);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(3);
+
+    print(vec);
+    cout<<removeDuplicates(vec)<<endl;
+    print(vec);
 }
