45b204654b1d6e896fcfa97bb49c541a2e5025fb
yyexela <yyexela@disroot.org>
2019-11-04 17:32:33 -0700

    1137 works properly

--- N-th Tribonacci Number/code.cpp
+++ N-th Tribonacci Number/code.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    int tribonacci(int n) {
+   int tribonacci(int n){
         //base cases
-        if(n <= 0) return 0;
+        if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
-    }
+        int a1 = 0; //n-3
+        int a2 = 1; //n-2
+        int a3 = 1; //n-1
+        int a4 = a1+a2+a3; //n
+        n -= 3;
+        while(n > 0){
+            a1=a2;
+            a2=a3;
+            a3=a4;
+            a4 = a1+a2+a3;
+            --n;
+        }
+        return a4;
+   }
 };
 
 int main(){
