6e27bbb94cbf12b1a416087735d6825f3ae373e6
yyexela <yyexela@disroot.org>
2019-11-05 15:42:17 -0700

    code.cpp partially works
    the program works for 407/987 test cases, except for where the longest substring is within another substring that is tested, the file skips over that starting position

---
+++ Longest Substring Without Repeating Characters/code.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+class Solution {
+private:
+    bool DEBUG = true;
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> map;
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < s.size(); ++i){
+            if(DEBUG) cout<<"Adding: "<<s[i]<<endl;
+            auto search = map.find(s[i]);
+            if(search != map.end()){
+                // size: i - index
+                if(DEBUG) cout<<"Found "<<s[i]<<" twice"<<endl;
+                if(max < count) max = count;
+                count = 0;
+                map.clear();
+            }
+            map.insert({s[i],i});
+            ++count;
+        }
+        if(max < count) max = count;
+        return max;
+    }
+};
+
+int main(){
+    Solution sol;
+    cout<<sol.lengthOfLongestSubstring("dvdf")<<endl;
+    /*
+    cout<<sol.lengthOfLongestSubstring("abceed")<<endl;
+    cout<<sol.lengthOfLongestSubstring("bbbbb")<<endl;
+    cout<<sol.lengthOfLongestSubstring("abcabcbb")<<endl;
+    cout<<sol.lengthOfLongestSubstring("pwwkew")<<endl;
+    */
+}
